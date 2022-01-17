81789ceb18b96ab3434d4ab1725e1e3ebc91b693
rgeqnu <dltndls1204@gmail.com>
2022-01-17 22:44:00 +0900

    124나라 미완

--- src/CodingTest.java
+++ src/CodingTest.java
@@ -12,23 +12,24 @@ public class CodingTest {
 
     public static String solution(int n) {
         String answer = "";
+        String[] ans = {"0","1","2","4"};
         //받은 숫자를 나눠 줍시다
         if (n == 3) {
-            answer += 4;
+            answer = 4+answer;
         } else if (n < 4) {
-            answer += n;
+            answer = n+answer;
         } else {
             while (n / 3 > 0) {
                 System.out.println("실행");
                 if (n % 3 == 0) {
-                    answer += 4;
+                    answer = 4 + answer;
                     n = (n / 3) - 1;
                 } else {
-                    answer += n % 3;
+                    answer = n % 3 + answer;
                     n = n / 3;
                 }
                 if (n / 3 == 0) {
-                    answer += n;
+                    answer = n + answer;
                 }
             }
         }
