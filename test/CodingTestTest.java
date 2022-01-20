80022b13632febf23c24c87b3728037ed10e46ab
rgeqnu <dltndls1204@gmail.com>
2022-01-20 23:41:20 +0900

    124나라 완성 테스트에있음

--- test/CodingTestTest.java
+++ test/CodingTestTest.java
@@ -6,24 +6,16 @@ class CodingTestTest {
 
     @Test
     void main() {
-        assertEquals(solution(1),"1");
+        /*assertEquals(solution(1),"1");
         assertEquals(solution(17),"122");
         assertEquals(solution(30),"244");
         assertEquals(solution(3),"4");
-        assertEquals(solution(4),"11");
-        System.out.println(solution(302));
+        assertEquals(solution(4),"11");;;;;;
+        System.out.println(solution(302));*/
     }
 
-    public String solution(int n) {
-        String answer = "";
-        String[] ans = {"4","1","2"};
-        //받은 숫자를 나눠 줍시다
-        while (n > 0) {
-            int a = n % 3;
-            answer = ans[a] + answer;
-            if (a == 0) n--;
-            n/=3;
-        }
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
         return answer;
     }
 }
