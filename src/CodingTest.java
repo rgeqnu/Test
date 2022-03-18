d2cbcde3f60e2bfa5faaffab3d5a3bf5c3e8fc46
rgeqnu <dltndls1204@gmail.com>
2022-03-18 22:19:10 +0900

    미오나

--- src/CodingTest.java
+++ src/CodingTest.java
@@ -1,5 +1,6 @@
 import javax.swing.plaf.synth.SynthToolTipUI;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 
@@ -7,18 +8,25 @@ public class CodingTest {
     public static void main(String[] args) {
 
         String[] id_list = {"muzi", "frodo", "apeach", "neo"};
-        String[] report = {"muzi frodo","apeach frodo","apeach frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
         solution(id_list,report,2);
 
     }
 
     public static int[] solution(String[] id_list, String[] report, int k){
         int[] answer = {};
+        int[] nut = new int[id_list.length];
+        Arrays.fill(nut,0);
         int a = 0;
         /*일단 중복제거
-        *
-        *
-        *
+        *확인해야할게 k넘는게 있는지
+        *있다면? > 그아이디 확인
+        *신고한사람을 찾아야지 레포트배열에서
+        *윗내용은 어제생각한거
+        * 바꾸는 이유 너무 비효율적임 비교를 몇번하는거야 또 스플릿에 나눠야되고
+        * 바꿔야될거같은게 해시이용해서 누가누구를 신고했는지 리스트에 저장
+        * 마지막에 리스트 확인해서 그사람 있으면 메일전송횟수 증가 시키면
+        * ㄴㄴ그냥 바이너리서치하면됨
         * */
 
         for (int i = 0; i < report.length; i++) {
@@ -34,8 +42,30 @@ public class CodingTest {
                 }
             }
         }
+        for (int i = 0; i < report.length; i++) {
+            if (!report[i].equals("x")) {
+                String[] split = report[i].split(" ");
+                int num = Arrays.binarySearch(id_list, split[1]);
+                if (num==-nut.length) {
+                    nut[0]++;
+                }else{
+                    nut[num]++;
+                }
+            }
+        }
+        for (int i = 0; i < id_list.length; i++) {
+            if (nut[i]>=k) {
+
+            }
+            if (!report[i].equals("x")) {
+                String[] split = report[i].split(" ");
+
+                }
+            }
+
+
         System.out.println("a의 값"+a);
-        for (String s : report) {
+        for (int s : nut) {
             System.out.println(s);
         }
         return answer;
