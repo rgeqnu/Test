a5e4f5609180e91dec0665c218ed8668650d804e
rgeqnu <dltndls1204@gmail.com>
2022-06-04 01:07:57 +0900

    미오나

--- src/CodingTest.java
+++ src/CodingTest.java
@@ -16,27 +16,11 @@ public class CodingTest {
     public static int[] solution(String[] id_list, String[] report, int k){
         int[] answer = {};
         int[] nut = new int[id_list.length];
-        Arrays.fill(nut,0);
-        int a = 0;
-        /*일단 중복제거
-        *확인해야할게 k넘는게 있는지
-        *있다면? > 그아이디 확인
-        *신고한사람을 찾아야지 레포트배열에서
-        *윗내용은 어제생각한거
-        * 바꾸는 이유 너무 비효율적임 비교를 몇번하는거야 또 스플릿에 나눠야되고
-        * 바꿔야될거같은게 해시이용해서 누가누구를 신고했는지 리스트에 저장
-        * 마지막에 리스트 확인해서 그사람 있으면 메일전송횟수 증가 시키면
-        * ㄴㄴ그냥 바이너리서치하면
-        * */
-
+        int[] result = new int[id_list.length];
         for (int i = 0; i < report.length; i++) {
-            if (report[i].equals("x")) {
-                System.out.println("확인");
-            }else {
-                a++;
+            if (!report[i].equals("x")) {
                 for (int j = i + 1; j < report.length; j++) {
                     if (report[i].equals(report[j])) {
-                        System.out.println(report[j]);
                         report[j] = "x";
                     }
                 }
@@ -45,30 +29,27 @@ public class CodingTest {
         for (int i = 0; i < report.length; i++) {
             if (!report[i].equals("x")) {
                 String[] split = report[i].split(" ");
-                int num = Arrays.binarySearch(id_list, split[1]);
-                if (num==-nut.length) {
-                    nut[0]++;
-                }else{
-                    nut[num]++;
-                }
+                nut[Arrays.asList(id_list).indexOf(split[1])]++;
             }
         }
         for (int i = 0; i < id_list.length; i++) {
             if (nut[i]>=k) {
-
-            }
-            if (!report[i].equals("x")) {
-                String[] split = report[i].split(" ");
-
+                for (int j = 0; j < report.length; j++) {
+                    if (!report[j].equals("x")) {
+                        String[] split = report[j].split(" ");
+                        if (split[1].equals(id_list[i])) {
+                            result[Arrays.asList(id_list).indexOf(split[0])]++;
+                        }
+                    }
                 }
             }
+            }
 
 
-        System.out.println("a의 값"+a);
-        for (int s : nut) {
+        for (int s : result) {
             System.out.println(s);
         }
-        return answer;
+        return result;
         }
 
     }
